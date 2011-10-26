4847f1d4a75b0b396d7b21eace73de42b172e390
Jasper Frumau <jasperfrumau@gmail.com>
2011-10-26 05:59:43 +0400

    modified:   functions/seo-central.php

--- functions/seo-central.php
+++ functions/seo-central.php
@@ -1,31 +1,26 @@
 <?php
-function seocentral(){
-?>
-Hello, world!<br />
-Hello, world!<br />
-Hello, world!
-<?php
-}
+
 //add_meta_box( $id, $title, $callback, $page, $context, $priority, $callback_args );
-add_meta_box("seocentralid", __('Say Hello', 'yourplugin'), "seocentral", "yourplugin");
-do_meta_boxes('yourplugin','advanced',null);
-?>
+//define custom box
 
-<script type=javascript>
-jQuery(document).ready( function($) {
-	// close postboxes that should be closed
-	jQuery('.if-js-closed').removeClass('if-js-closed').addClass('closed');
+// backwards compatible (before WP 3.0)
+// add_action( 'admin_init', 'seocentral_add_custom_box', 1 );
 
-	// postboxes
-	<?php
-	global $wp_version;
-	if(version_compare($wp_version,"2.7-alpha", "<")){
-		echo "add_postbox_toggles('yourplugin_helloworld_meta_box');"; //For WP2.6 and below
-	}
-	else{
-		echo "postboxes.add_postbox_toggles('seocentral');"; //For WP2.7 and above
-	}
-	?>
+/* Do something with the data entered */
+add_action( 'save_post', 'seocentral_save_postdata' );
 
-});
-</script>
+/* Adds a box to the main column on the Post and Page edit screens */
+function seocentral_add_custom_box() {
+    add_meta_box( 
+        'seocentral_sectionid', // HTML ID
+        __( 'My Post Section Title', 'seocentral_textdomain' ), // Title
+        'myplugin_inner_custom_box', // call back
+        'post' // On what Page?
+    );
+    add_meta_box(
+        'myplugin_sectionid',
+        __( 'My Post Section Title', 'seocentral_textdomain' ), 
+        'seocentral_inner_custom_box',
+        'page'
+    );
+}
